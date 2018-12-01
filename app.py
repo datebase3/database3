a9639a87d8f2004d7879f216dfe4b86214e706cc
ziyun zhou <1304178462@qq.com>
2018-12-01 15:04:21 +0800

    测试网页

--- app.py
+++ app.py
@@ -5,6 +5,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    return render_template('index.html')
+
+@app.route('/header.html')
+def navigation_bar():
+    return render_template('header.html')
+
+@app.route('/recommend.html')
+def recommend():
     if 'search' in session:
         result = "???"
     else:
@@ -21,8 +29,36 @@ def index():
             'author': 4
         }
     ]
-    return render_template('index.html',result = result)
+    return render_template('recommend.html',result = result)
 
+@app.route('/test.html')
+def test():
+    return render_template('test.html')
+
+@app.route('/search.html')
+def go_search():
+    return render_template('search.html')
+
+@app.route('/result',methods=['GET', 'POST'])
+def search():
+    if request.method!="POST":
+        return render_template('recommend.html')
+    if request.form['search_con']=="":
+        return render_template('recommend.html')
+    search_con = request.form['search_con']
+    result = [
+        {
+            'name': 1,
+            'author': 2
+        }, {
+            'name': 2,
+            'author': 3
+        }, {
+            'name': 3,
+            'author': 4
+        }
+    ]
+    return render_template('result.html',result = result)
 
 if __name__ == '__main__':
     app.run(debug = True)
