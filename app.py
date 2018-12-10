736f697aa3563b401e36a1f25877d1a414177647
ziyun zhou <1304178462@qq.com>
2018-12-10 22:22:35 +0800

    提交

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from flask import Flask, session, redirect, url_for, escape, request
 from flask import render_template
 app = Flask(__name__)
@@ -5,7 +8,25 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('login.html')
+
+@app.route('/login',methods=['GET', 'POST'])
+def login():
+    user = request.form['user']
+    password = request.form['password']
+    if request.form['login'] == u"登录":
+        if True:#login_user(user,password):#测试用户是否正确
+            session["user"] = user
+            return render_template('index.html')
+        else:
+            return render_template('login.html')
+    elif request.form['login'] == u"注册":
+        if True:#test_user(user):#测试用户是否存在
+            return render_template('login.html')
+        else:
+            #insert_user(user,password)#添加用户
+            session["user"] = user
+            return render_template('index.html')
 
 @app.route('/header.html')
 def navigation_bar():
@@ -13,23 +34,23 @@ def navigation_bar():
 
 @app.route('/recommend.html')
 def recommend():
-    if 'search' in session:
-        result = "???"
+    if session.get("user"):
+        result = None#getbooksbyuser(session.get("user"))#通过用户名获取浏览记录，通过看过书的标签获取推荐书单
+        return render_template('recommend.html', result=result)
     else:
-        result = "!!!"
-    result = [
-        {
-            'name': 1,
-            'author': 2
-        },{
-            'name': 2,
-            'author': 3
-        },{
-            'name': 3,
-            'author': 4
-        }
-    ]
-    return render_template('recommend.html',result = result)
+        return render_template('login.html')
+
+@app.route('/introduction',methods=['GET', 'POST'])
+def introduction():
+    book = request.args.get('book')
+    author = request.args.get('author')
+    if book == "0":
+        result = None#getIntroByAuthor(author)
+    elif author == "0":
+        result = None#getIntroByBook(book)
+    else:
+        return redirect(url_for('recommend'))
+    return render_template('introduction.html',result=result)
 
 @app.route('/test.html')
 def test():
@@ -46,18 +67,7 @@ def search():
     if request.form['search_con']=="":
         return render_template('recommend.html')
     search_con = request.form['search_con']
-    result = [
-        {
-            'name': 1,
-            'author': 2
-        }, {
-            'name': 2,
-            'author': 3
-        }, {
-            'name': 3,
-            'author': 4
-        }
-    ]
+    result = None#getbooksbyfind(search_con)#通过搜索内容获取书单
     return render_template('result.html',result = result)
 
 if __name__ == '__main__':
